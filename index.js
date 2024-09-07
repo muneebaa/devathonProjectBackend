0e303e75cc9b902cd34e02f881dad2e8ce183244
rohailtaha <taharohail77@gmail.com>
2024-09-07 12:09:31 +0500

    feat: add api for getting doctors and setting availability

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const { apiRouter } = require('./routes/api-router');
 app.use(cors());
 
 app.use(express.json());
+
 app.use(
   express.urlencoded({
     extended: true,
@@ -31,6 +32,7 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(authRouter);
 app.use('/api', apiRouter);
 
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,8 +1,48 @@
 const router = require('express').Router();
+const { addAppointment } = require('../controllers/appointment');
+const { addDoctorAvailability, getDoctors } = require('../controllers/user');
+const { API_ERRORS, USER_ROLE } = require('../lib/constants');
+const { serverErrorResponse } = require('../lib/utils');
+const { isAuthenticated } = require('../middlewares/authentication');
+const { roleAuthorization } = require('../middlewares/authorization');
 
 router.get('/test', (req, res) => {
   return res.json({ hello: 'test api' });
 });
+
+// doctor sets his appointments
+router.post(
+  '/doctors/:doctorId/availability',
+  isAuthenticated,
+  roleAuthorization([USER_ROLE.Doctor]),
+  async (req, res) => {
+    // TODO: perform validation
+    try {
+      const doctorId = req.params.doctorId;
+      await addDoctorAvailability(doctorId, req.body);
+      return res.status(201).end();
+    } catch (error) {
+      console.error(error);
+      return serverErrorResponse(res);
+    }
+  }
+);
+
+// patient sees doctors availability
+router.get(
+  '/doctors',
+  isAuthenticated,
+  roleAuthorization([USER_ROLE.Patient]),
+  async (req, res) => {
+    try {
+      const doctors = await getDoctors();
+      return res.json({ doctors }).end();
+    } catch (error) {
+      console.error(error);
+      return serverErrorResponse(res);
+    }
+  }
+);
 
 module.exports = {
   apiRouter: router,
